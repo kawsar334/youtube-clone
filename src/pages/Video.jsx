f05a112595a5f711ebc405e2633bd3f51703e199
kawsar <kawsarfiroz11@gmail.com>
2023-02-18 09:21:49 +0300

    added a video

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -116,7 +116,7 @@ const Video = () => {
     <Container>
 <Content>
     <VideoWrapper>
-                  <iframe width="100%" height="450" src="https://www.youtube.com/embed/yIaXoop8gl4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> 
+        <video src="/w.mp4" style={{width:"100%", height:"100%", objectFit:"cover"}} controls />
     </VideoWrapper>
     <Title>Python bangla Tutorial . </Title>
     <Details>
